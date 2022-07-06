82c0a3d4906b0cfdcc1f8b5955d336e623daa5ef
Sarah Vessels <cheshire137@gmail.com>
2022-07-05 19:29:12 -0500

    Prompt user to choose a bridge

--- cmd/gohuedata/main.go
+++ cmd/gohuedata/main.go
@@ -2,9 +2,55 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/cheshire137/gohuedata/pkg/config"
+	"github.com/cheshire137/gohuedata/pkg/hueapi"
 )
+
+func displayBridgePrompt(bridges []hueapi.Bridge) {
+	fmt.Println("\nChoose a Philips Hue bridge:")
+	for i, bridge := range bridges {
+		fmt.Printf("%d. %s -- %s via %s\n", i+1, bridge.Name, bridge.IPAddress, bridge.Username)
+	}
+}
+
+func isValidBridgeIndex(index int, bridges []hueapi.Bridge) bool {
+	return index >= 1 && index <= len(bridges)
+}
+
+func getBridgeSelectionFromUser(bridges []hueapi.Bridge) *hueapi.Bridge {
+	displayBridgePrompt(bridges)
+
+	userInput := ""
+	bridgeSelection := -1
+	invalidIndex := true
+	errorPrefix := "❌ Invalid bridge choice"
+	var err error
+
+	for userInput == "" || invalidIndex {
+		fmt.Scanln(&userInput)
+		bridgeSelection, err = strconv.Atoi(userInput)
+		nonIntegerInput := err != nil
+		invalidIndex = !isValidBridgeIndex(bridgeSelection, bridges)
+		if nonIntegerInput {
+			fmt.Println(errorPrefix + ":" + err.Error())
+		} else if invalidIndex {
+			if len(bridges) > 1 {
+				fmt.Println(errorPrefix+", must be between 1 and", len(bridges))
+			} else {
+				fmt.Println(errorPrefix + ", must be 1")
+			}
+		}
+		if nonIntegerInput || invalidIndex {
+			displayBridgePrompt(bridges)
+			userInput = ""
+		}
+	}
+
+	bridgeIndex := bridgeSelection - 1
+	return &bridges[bridgeIndex]
+}
 
 func main() {
 	config, err := config.NewConfig("config.yml")
@@ -12,10 +58,8 @@ func main() {
 		fmt.Println("❌ Failed to load configuration:", err)
 		return
 	}
-
 	fmt.Println("✅ Loaded configuration")
-	fmt.Println("\nPhilips Hue bridges:")
-	for i, bridge := range config.Bridges {
-		fmt.Printf("%d. %s -- %s via %s\n", i+1, bridge.Name, bridge.IPAddress, bridge.Username)
-	}
+
+	bridge := getBridgeSelectionFromUser(config.Bridges)
+	fmt.Println("✅ Selected bridge:", bridge.Name)
 }
