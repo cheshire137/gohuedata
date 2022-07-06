89773521aae3a64888ca47f75e0769daa3a3e22c
Sarah Vessels <cheshire137@gmail.com>
2022-07-05 19:59:41 -0500

    Add bridge GetUrl

--- pkg/hueapi/bridge.go
+++ pkg/hueapi/bridge.go
@@ -1,7 +1,13 @@
 package hueapi
 
+import "net/url"
+
 type Bridge struct {
 	Name      string `yaml:"name"`
 	IPAddress string `yaml:"ip_address"`
 	Username  string `yaml:"username"`
+}
+
+func (b *Bridge) GetUrl() (*url.URL, error) {
+	return url.Parse("http://" + b.IPAddress)
 }
