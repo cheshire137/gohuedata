c239caa24c9205efbdf555b2c798386eed9ca49c
Sarah Vessels <cheshire137@gmail.com>
2022-12-23 18:26:17 -0600

    Add PageInfo

---
+++ pkg/server/pagination.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"net/url"
+	"strconv"
+)
+
+type PageInfo struct {
+	Page    int
+	PerPage int
+}
+
+func GetPageInfo(url *url.URL) (*PageInfo, error) {
+	pageStr := url.Query().Get("page")
+	var page int
+	var err error
+	if pageStr == "" {
+		page = 1
+	} else {
+		if page, err = strconv.Atoi(pageStr); err != nil {
+			return nil, err
+		}
+	}
+	perPageStr := url.Query().Get("per_page")
+	var perPage int
+	if perPageStr == "" {
+		perPage = 10
+	} else {
+		if perPage, err = strconv.Atoi(perPageStr); err != nil {
+			return nil, err
+		}
+	}
+	return &PageInfo{Page: page, PerPage: perPage}, nil
+}
