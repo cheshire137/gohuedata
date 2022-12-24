a5ce5a535b8b865f1f8b0214c1664532c9cfdeec
Sarah Vessels <cheshire137@gmail.com>
2022-12-23 18:27:02 -0600

    Take page, per_page; return page, totalPages

--- pkg/server/get_temperature_readings_handler.go
+++ pkg/server/get_temperature_readings_handler.go
@@ -4,16 +4,41 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/cheshire137/gohuedata/pkg/data_store"
 	"github.com/cheshire137/gohuedata/pkg/util"
 )
 
+type TemperatureReadingsResponse struct {
+	TemperatureReadings []*data_store.TemperatureReading `json:"temperatureReadings"`
+	Page                int                              `json:"page"`
+	TotalPages          int                              `json:"totalPages"`
+}
+
 func (e *Env) GetTemperatureReadingsHandler(w http.ResponseWriter, r *http.Request) {
-	page := 1
-	tempReadings, err := e.ds.LoadTemperatureReadings(page)
+	pageInfo, err := GetPageInfo(r.URL)
 	if err != nil {
 		util.ErrorJson(w, err)
 		return
 	}
+
+	tempReadings, err := e.ds.LoadTemperatureReadings(pageInfo.Page, pageInfo.PerPage)
+	if err != nil {
+		util.ErrorJson(w, err)
+		return
+	}
+
+	totalTempReadings, err := e.ds.TotalTemperatureReadings()
+	if err != nil {
+		util.ErrorJson(w, err)
+		return
+	}
+
+	response := TemperatureReadingsResponse{
+		TemperatureReadings: tempReadings,
+		Page:                pageInfo.Page,
+		TotalPages:          util.TotalPages(totalTempReadings, pageInfo.PerPage),
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tempReadings)
+	json.NewEncoder(w).Encode(response)
 }
