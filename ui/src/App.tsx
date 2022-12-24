fbab49a07aad451789e4c357bfaab14c84f4013b
Sarah Vessels <cheshire137@gmail.com>
2022-12-24 15:51:37 -0600

    Start on display of temp data in frontend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
+import GoHueDataApi from './models/GoHueDataApi';
+import useGetTemperatureReadings from './hooks/use-get-temperature-readings';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const api = new GoHueDataApi(8080);
+  const { temperatureReadings, fetching, error } = useGetTemperatureReadings(api);
+
+  if (fetching) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  if (!temperatureReadings) {
+    return <p>No data</p>;
+  }
+
+  return <div className="App">
+    {temperatureReadings.map(tempReading => <p key={tempReading.id()}>
+      {tempReading.temperature}&deg; {tempReading.units} as of {tempReading.lastUpdated}
+    </p>)}
+  </div>;
 }
 
 export default App;
