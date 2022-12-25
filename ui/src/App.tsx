730114ac4fb30b751a72ffdb6166ce604043c9a0
Sarah Vessels <cheshire137@gmail.com>
2022-12-24 19:34:52 -0600

    Pull out TemperatureReadings component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import useGetTemperatureReadings from './hooks/use-get-temperature-readings';
+import TemperatureReadings from './components/TemperatureReadings';
 import './App.css';
 
 function App() {
-  const { temperatureReadings, fetching, error } = useGetTemperatureReadings({
-    page: 1,
-    perPage: 100,
-  });
-
-  if (fetching) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  if (!temperatureReadings) {
-    return <p>No data</p>;
-  }
-
-  return <ul>
-    {temperatureReadings.map(tempReading => <li key={tempReading.id()}>
-      ({tempReading.sensor.bridge.name}) {tempReading.sensor.name}: {tempReading.temperature}&deg; {tempReading.units} as of {tempReading.lastUpdated}
-    </li>)}
-  </ul>;
+  return <TemperatureReadings />;
 }
 
 export default App;
---
+++ ui/src/components/TemperatureReadings.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import useGetTemperatureReadings from '../hooks/use-get-temperature-readings';
+
+const TemperatureReadings = () => {
+  const { temperatureReadings, fetching, error } = useGetTemperatureReadings({
+    page: 1,
+    perPage: 100,
+  });
+
+  if (fetching) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  if (!temperatureReadings) {
+    return <p>No data</p>;
+  }
+
+  return <ul>
+    {temperatureReadings.map(tempReading => <li key={tempReading.id()}>
+      ({tempReading.sensor.bridge.name}) {tempReading.sensor.name}: {tempReading.temperature}&deg; {tempReading.units} as of {tempReading.lastUpdated}
+    </li>)}
+  </ul>;
+};
+
+export default TemperatureReadings;
