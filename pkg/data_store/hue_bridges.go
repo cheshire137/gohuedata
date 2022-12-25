c239ff4de739bccf4adb803bfab97fff821faad5
Sarah Vessels <cheshire137@gmail.com>
2022-12-24 19:09:07 -0600

    Add LoadHueBridgesByIPAddress

--- pkg/data_store/hue_bridges.go
+++ pkg/data_store/hue_bridges.go
@@ -1,6 +1,8 @@
 package data_store
 
 import (
+	"strings"
+
 	"github.com/cheshire137/gohuedata/pkg/hue_api"
 )
 
@@ -16,4 +18,27 @@ func (ds *DataStore) AddHueBridge(bridge *hue_api.Bridge) error {
 		return err
 	}
 	return nil
+}
+
+func (ds *DataStore) LoadHueBridgesByIPAddress(ipAddresses ...string) ([]*HueBridge, error) {
+	bridges := []*HueBridge{}
+	if len(ipAddresses) == 0 {
+		return bridges, nil
+	}
+	queryStr := `SELECT ip_address, name FROM hue_bridges WHERE ip_address IN (?` +
+		strings.Repeat(", ?", len(ipAddresses)-1) + `)`
+	rows, err := ds.db.Query(queryStr, ipAddresses)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		bridge := &HueBridge{}
+		err = rows.Scan(&bridge.IPAddress, &bridge.Name)
+		if err != nil {
+			return nil, err
+		}
+		bridges = append(bridges, bridge)
+	}
+	return bridges, nil
 }
