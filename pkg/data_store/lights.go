0456496620a8dc508d3c7ef2fc4243e0227c86fd
Sarah Vessels <cheshire137@gmail.com>
2022-12-29 13:57:32 -0600

    Fix 3 values for 4 columns

--- pkg/data_store/lights.go
+++ pkg/data_store/lights.go
@@ -3,7 +3,7 @@ package data_store
 import "github.com/cheshire137/gohuedata/pkg/hue_api"
 
 func (ds *DataStore) AddLight(bridge *hue_api.Bridge, id string, light *hue_api.Light) error {
-	insertQuery := `INSERT INTO lights (unique_id, id, name, bridge_ip_address) VALUES (?, ?, ?)
+	insertQuery := `INSERT INTO lights (unique_id, id, name, bridge_ip_address) VALUES (?, ?, ?, ?)
 		ON CONFLICT(unique_id)
 		DO UPDATE SET id = excluded.id, name = excluded.name, bridge_ip_address = excluded.bridge_ip_address`
 	stmt, err := ds.db.Prepare(insertQuery)
