966011e5b65af55e920b58f263d8e338cc084547
Sarah Vessels <cheshire137@gmail.com>
2022-12-29 13:35:33 -0600

    Move create table functions onto DataStore

--- pkg/data_store/create_tables.go
+++ pkg/data_store/create_tables.go
@@ -1,24 +1,22 @@
 package data_store
 
-import "database/sql"
-
 func (ds *DataStore) CreateTables() error {
-	err := createTemperatureSensorsTable(ds.db)
+	err := ds.createTemperatureSensorsTable()
 	if err != nil {
 		return err
 	}
-	err = createTemperatureReadingsTable(ds.db)
+	err = ds.createTemperatureReadingsTable()
 	if err != nil {
 		return err
 	}
-	err = createLightsTable(ds.db)
+	err = ds.createLightsTable()
 	if err != nil {
 		return err
 	}
-	return createHueBridgesTable(ds.db)
+	return ds.createHueBridgesTable()
 }
 
-func createTemperatureReadingsTable(db *sql.DB) error {
+func (ds *DataStore) createTemperatureReadingsTable() error {
 	createTableQuery := `CREATE TABLE IF NOT EXISTS temperature_readings (
 		temperature_sensor_id TEXT NOT NULL,
 		last_updated TEXT NOT NULL,
@@ -26,7 +24,7 @@ func createTemperatureReadingsTable(db *sql.DB) error {
 		units TEXT NOT NULL,
 		PRIMARY KEY (temperature_sensor_id, last_updated, temperature, units)
 	)`
-	stmt, err := db.Prepare(createTableQuery)
+	stmt, err := ds.db.Prepare(createTableQuery)
 	if err != nil {
 		return err
 	}
@@ -37,12 +35,12 @@ func createTemperatureReadingsTable(db *sql.DB) error {
 	return nil
 }
 
-func createHueBridgesTable(db *sql.DB) error {
+func (ds *DataStore) createHueBridgesTable() error {
 	createTableQuery := `CREATE TABLE IF NOT EXISTS hue_bridges (
 		ip_address TEXT PRIMARY KEY,
 		name TEXT NOT NULL
 	)`
-	stmt, err := db.Prepare(createTableQuery)
+	stmt, err := ds.db.Prepare(createTableQuery)
 	if err != nil {
 		return err
 	}
@@ -53,13 +51,13 @@ func createHueBridgesTable(db *sql.DB) error {
 	return nil
 }
 
-func createLightsTable(db *sql.DB) error {
+func (ds *DataStore) createLightsTable() error {
 	createTableQuery := `CREATE TABLE IF NOT EXISTS lights (
 		id TEXT PRIMARY KEY,
 		name TEXT NOT NULL,
 		bridge_ip_address TEXT NOT NULL
 	)`
-	stmt, err := db.Prepare(createTableQuery)
+	stmt, err := ds.db.Prepare(createTableQuery)
 	if err != nil {
 		return err
 	}
@@ -68,7 +66,7 @@ func createLightsTable(db *sql.DB) error {
 		return err
 	}
 	createIndexQuery := `CREATE UNIQUE INDEX IF NOT EXISTS idx_lights_id_bridge_ip ON lights (id, bridge_ip_address)`
-	stmt, err = db.Prepare(createIndexQuery)
+	stmt, err = ds.db.Prepare(createIndexQuery)
 	if err != nil {
 		return err
 	}
@@ -79,13 +77,13 @@ func createLightsTable(db *sql.DB) error {
 	return nil
 }
 
-func createTemperatureSensorsTable(db *sql.DB) error {
+func (ds *DataStore) createTemperatureSensorsTable() error {
 	createTableQuery := `CREATE TABLE IF NOT EXISTS temperature_sensors (
 		id TEXT PRIMARY KEY,
 		name TEXT NOT NULL,
 		bridge_ip_address TEXT NOT NULL
 	)`
-	stmt, err := db.Prepare(createTableQuery)
+	stmt, err := ds.db.Prepare(createTableQuery)
 	if err != nil {
 		return err
 	}
@@ -95,7 +93,7 @@ func createTemperatureSensorsTable(db *sql.DB) error {
 	}
 	createIndexQuery := `CREATE UNIQUE INDEX IF NOT EXISTS idx_temperature_sensors_id_bridge_ip
 		ON temperature_sensors (id, bridge_ip_address)`
-	stmt, err = db.Prepare(createIndexQuery)
+	stmt, err = ds.db.Prepare(createIndexQuery)
 	if err != nil {
 		return err
 	}
