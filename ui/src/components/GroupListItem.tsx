fb2986b9bd880ef30b321ae5380cb7c631436336
Sarah Vessels <cheshire137@gmail.com>
2023-12-16 14:03:00 -0600

    Link to each group via list item

--- ui/src/components/GroupListItem.tsx
+++ ui/src/components/GroupListItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box } from '@primer/react';
+import { Box, Link } from '@primer/react';
 import Group from '../models/Group';
 import BridgeDisplay from './BridgeDisplay';
+import { useHref } from 'react-router-dom';
 
 interface Props {
   group: Group;
@@ -9,7 +10,9 @@ interface Props {
 
 const GroupListItem = ({ group }: Props) => {
   return <Box as="li" mb={2}>
-    {group.name} &mdash; {group.totalLights} {group.totalLights === 1 ? 'light' : 'lights'}
+    <Link href={useHref(`/groups/${group.id}/?bridge=${encodeURIComponent(group.bridge.name)}`)}>
+      {group.name} &mdash; {group.totalLights} {group.totalLights === 1 ? 'light' : 'lights'}
+    </Link>
     <BridgeDisplay bridge={group.bridge} />
   </Box>;
 };
