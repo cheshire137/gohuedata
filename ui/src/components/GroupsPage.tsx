d3e5bdab3baaf5512035495363a255243e1b5860
Sarah Vessels <cheshire137@gmail.com>
2023-12-16 14:53:13 -0600

    Don't need to set intermediate groups title

--- ui/src/components/GroupsPage.tsx
+++ ui/src/components/GroupsPage.tsx
@@ -1,12 +1,7 @@
-import React, { useContext, useEffect } from 'react';
-import { PageContext } from '../contexts/PageContext';
+import React from 'react';
 import GroupList from './GroupList';
 
 const GroupsPage = () => {
-  const { setPageTitle } = useContext(PageContext);
-
-  useEffect(() => setPageTitle('Groups'), [setPageTitle]);
-
   return <GroupList />;
 };
 
