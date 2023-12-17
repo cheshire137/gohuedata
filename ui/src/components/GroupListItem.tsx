233c1708565f65867c01c8fbe9ca4308ef418a2b
Sarah Vessels <cheshire137@gmail.com>
2023-12-16 18:12:36 -0600

    Fix group page URL in link

--- ui/src/components/GroupListItem.tsx
+++ ui/src/components/GroupListItem.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GroupListItem = ({ group }: Props) => {
   return <Box as="li" mb={2}>
-    <Link href={useHref(`/groups/${group.id}/?bridge=${encodeURIComponent(group.bridge.name)}`)}>
+    <Link href={useHref(`/group/${group.id}/?bridge=${encodeURIComponent(group.bridge.name)}`)}>
       {group.name} &mdash; {group.totalLights} {group.totalLights === 1 ? 'light' : 'lights'}
     </Link>
     <BridgeDisplay bridge={group.bridge} />
