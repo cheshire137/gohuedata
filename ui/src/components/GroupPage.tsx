935e0c6e0ff988a9608ea00ffe6b0cb17fae0a9c
Sarah Vessels <cheshire137@gmail.com>
2023-12-16 19:13:56 -0600

    Split on vs off lights in group

--- ui/src/components/GroupPage.tsx
+++ ui/src/components/GroupPage.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { Box } from '@primer/react';
+import { Box, Heading } from '@primer/react';
 import { PageContext } from '../contexts/PageContext';
 import { useLoaderData } from 'react-router-dom';
 import GroupExtended from '../models/GroupExtended';
@@ -12,7 +12,9 @@ const GroupPage = () => {
 
   useEffect(() => setPageTitle(`Group: ${group.name}`), [setPageTitle, group.name]);
 
-  console.log('group', group)
+  const onLights = group.lights.filter(light => light.latestState.on);
+  const offLights = group.lights.filter(light => !light.latestState.on);
+
   return <div>
     <div>
       Type: <Box fontSize={1} display="inline-block" color="fg.muted">{group.type}</Box> / Class: <Box fontSize={1} display="inline-block" color="fg.muted">{group.groupClass}</Box> / Bridge: <BridgeDisplay bridge={group.bridge} />
@@ -23,7 +25,10 @@ const GroupPage = () => {
       <dt># sensors</dt>
       <dd>{group.totalSensors}</dd>
     </dl>
-    <LightList lights={group.lights} />
+    <Heading as="h2">On</Heading>
+    <LightList lights={onLights} />
+    <Heading as="h2">Off</Heading>
+    <LightList lights={offLights} />
   </div>;
 };
 
